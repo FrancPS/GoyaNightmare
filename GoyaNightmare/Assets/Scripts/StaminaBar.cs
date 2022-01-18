a8eca4076e3bcbd972c31fbb5d5d0f6849b40518
FrancPS <fportasol@gmail.com>
2022-01-18 12:00:26 +0100

    StaminaBar Clean

--- GoyaNightmare/Assets/Scripts/StaminaBar.cs
+++ GoyaNightmare/Assets/Scripts/StaminaBar.cs
@@ -3,52 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/* 
- * This script is responsible for controlling the player stamina bar.
- * Works as a Singleton, to be able to acces script members and functions calling "StaminaBar.instance".
- * 'UseStamina()' must be called when performing any action that consumes stamina.
- * 'HasStamina()' can be called to check if there is any stamina left.
+/* This script is responsible for controlling the player stamina bar.
+ * It is a Singleton pattern, meant to be used (mostly) by the PlayerController.
+ * Stamina will be consumed upon calling UseStamina(), and will be recovered over time
+ * by the RegenStamina Coroutine.
+ * The bar will fade in on usage, and fade out when its filled completely again.
  */
 
 public class StaminaBar : MonoBehaviour
 {
-    public static StaminaBar instance; // Singleton pattern
+    // SINGLETON PATTERN
+    public static StaminaBar Instance => instance;
+    private static StaminaBar instance;
 
-    public float regenDelayTime;
-    public float regenSpeedRatio;
-    public Image background;
-    public Image fill;
-    public float fadeTime;
+    // Regenerate Stamina Coroutine
+    public float regenDelaySeconds;     // Time before starting the regeneration.
+    public float regenTickSeconds;      // Time between coroutine ticks.
+    private WaitForSeconds regenTick;
+    public float fadeDelaySeconds;     // Time before fading out the stamina bar, when on full stamina.
+    private Coroutine regen;
 
+    // Hide Show Coroutine
+    public float fadeDuration;
+
+    // Slider refs
     private Slider staminaBar;
+    private Image background;
+    private Image fill;
+
+    // Stamina values
     private float maxStamina;
     private float currentStamina;
-    private Coroutine regen;
-    private WaitForSeconds regenTick;
-    private YieldInstruction fadeInstruction = new YieldInstruction();
 
     void Awake()
     {
-        staminaBar = gameObject.GetComponent<Slider>();
+        // Singleton!
         instance = this;
-        regenTick = new WaitForSeconds(regenSpeedRatio);
+
+        // Slider refs
+        staminaBar = gameObject.GetComponent<Slider>();
+        fill = staminaBar.fillRect.GetComponent<Image>();
+        background = staminaBar.transform.Find("Background").GetComponent<Image>();
+
+        // Since we would be calling this WaitForSeconds in a while loop,
+        // we instead store its reference so we don't have to create it every time.
+        regenTick = new WaitForSeconds(regenTickSeconds);
     }
 
     void Start()
     {
-        // Initialise bar
+        // Sync logic with the Slider component at 100%.
         maxStamina = staminaBar.maxValue;
         currentStamina = maxStamina;
-        staminaBar.maxValue = maxStamina;
         staminaBar.value = maxStamina;
 
-        // Set bar to invisible
-        Color bgColor = background.color;
-        Color fillColor = fill.color;
-        bgColor.a = 0f;
-        fillColor.a = 0f;
-        background.color = bgColor;
-        fill.color = fillColor;
+        // Start the Level with a clean screen.
+        HideShowBarInstantly(false);
     }
 
     public bool HasStamina()
@@ -63,13 +73,15 @@ public class StaminaBar : MonoBehaviour
         if (currentStamina < 0) currentStamina = 0;
         staminaBar.value = currentStamina;
 
+        // Defense vs UseStamina spamming.
+        // By storing the coroutine's reference, each time we UseStamina we can restart the regen process.
         if (regen != null) StopCoroutine(regen);
         regen = StartCoroutine(RegenStamina());
     }
 
     private IEnumerator RegenStamina()
     {
-        yield return new WaitForSeconds(regenDelayTime);
+        yield return new WaitForSeconds(regenDelaySeconds);
 
         while (currentStamina < maxStamina)
         {
@@ -78,9 +90,11 @@ public class StaminaBar : MonoBehaviour
 
             yield return regenTick;
         }
-        yield return new WaitForSeconds(regenDelayTime);
-        StartCoroutine(HideShowBar(false));
+
+        yield return new WaitForSeconds(fadeDelaySeconds);
+
         regen = null;
+        StartCoroutine(HideShowBar(false));
     }
 
     private IEnumerator HideShowBar(bool mustShow)
@@ -89,25 +103,37 @@ public class StaminaBar : MonoBehaviour
         Color bgColor = background.color;
         Color fillColor = fill.color;
 
-        // If must show and it is already shown, return.
+        // If must show but it is already there, return.
         if (bgColor.a >= 1f && mustShow) yield break;
 
-        while (elapsedTime < fadeTime)
+        while (elapsedTime < fadeDuration)
         {
-            yield return fadeInstruction;
+            yield return null;
             elapsedTime += Time.deltaTime;
-            
+
             if (mustShow)
             {
-                bgColor.a = Mathf.Clamp01(elapsedTime / fadeTime);
-                fillColor.a = Mathf.Clamp01(elapsedTime / fadeTime);
-            } else
-            {
-                bgColor.a = 1f - Mathf.Clamp01(elapsedTime / fadeTime);
-                fillColor.a = 1f - Mathf.Clamp01(elapsedTime / fadeTime);
+                bgColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+                fillColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
             }
+            else
+            {
+                bgColor.a = 1f - Mathf.Clamp01(elapsedTime / fadeDuration);
+                fillColor.a = 1f - Mathf.Clamp01(elapsedTime / fadeDuration);
+            }
+
             background.color = bgColor;
             fill.color = fillColor;
         }
+    }
+
+    private void HideShowBarInstantly(bool mustShow)
+    {
+        Color bgColor = background.color;
+        Color fillColor = fill.color;
+        bgColor.a = mustShow ? 1f : 0f;
+        fillColor.a = mustShow ? 1f : 0f;
+        background.color = bgColor;
+        fill.color = fillColor;
     }
 }
